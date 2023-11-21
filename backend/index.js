f3ea2dc7ba78910f85117e31e14e0772061421d6
Jason Rahm <jasonrahm@gmail.com>
2023-11-20 22:41:25 -0700

    backend starter files copied over

---
+++ backend/index.js
@@ -0,0 +1,61 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const dal = require('./dal.js')
+
+const port = 3000
+
+app.use(express.static('public'))
+app.use(cors())
+
+// create account
+app.get('/account/create/:name/:email/:password', function (req, res) {
+  const { name, email, password } = req.params
+  dal.create(name, email, password).then((user) => {
+    console.log(user)
+    res.send(user)
+  })
+})
+
+// all accounts
+app.get('/account/all', function (req, res) {
+  dal.all().then((docs) => {
+    console.log(docs)
+    res.send(docs)
+  })
+})
+
+// login
+app.get('/account/login/:email/:password', function (req, res) {
+  res.send({
+    email: req.params.email,
+    password: req.params.password,
+  })
+})
+
+// deposit
+app.get('/account/deposit/:email/:amount', function (req, res) {
+  res.send({
+    email: req.params.email,
+    amount: req.params.amount,
+  })
+})
+
+// withdraw
+app.get('/account/withdraw/:email/:amount', function (req, res) {
+  res.send({
+    email: req.params.email,
+    amount: req.params.amount,
+  })
+})
+
+// balance
+app.get('/account/balance/:email', function (req, res) {
+  res.send({
+    email: req.params.email,
+    balance: 'beats me',
+  })
+})
+
+app.listen(port)
+console.log('Running on port: ' + port)
